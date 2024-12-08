acb01a668a04ed57cc3fbb3c65511f8969ba5571
regnna <ritmchats@gmail.com>
2024-12-08 23:32:58 +0530

    579. Find Cumulative Salary of an Employee

--- Premium/579. Find Cumulative Salaryof an Employee/query.py
+++ Premium/579. Find Cumulative Salaryof an Employee/query.py
@@ -1,12 +1,14 @@
-# to install: pip install duckdb
+"""
+Range between and as order by month it will check preceding months such as if the current month is 7 it will check 6 and 5 
+row between check preceding rows even if the preceding row having month 4 and 3 
+"""
+
 import pandas as pd
 import duckdb
 
 data = [[1, 1, 20], [2, 1, 20], [1, 2, 30], [2, 2, 30], [3, 2, 40], [1, 3, 40], [3, 3, 60], [1, 4, 60], [3, 4, 70], [1, 7, 90], [1, 8, 90]]
 employee = pd.DataFrame(data, columns=['id', 'month', 'salary']).astype({'id':'Int64', 'month':'Int64', 'salary':'Int64'})
 
-# Range between and as order by month it will check preceding months such as if the current month is 7 it will check 6 and 5 
-# row between check preceding rows even if the preceding row having month 4 and 3 
 
 print(duckdb.query("""
 with cte as(
