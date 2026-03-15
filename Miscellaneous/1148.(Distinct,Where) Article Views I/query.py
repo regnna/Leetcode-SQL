834dcdbd18a2d254556b16260f84fd57e2e8efda
Ritam Chatterjee <ritmchats@gmail.com>
2026-03-15 23:49:46 +0530

    Miscellaneous/1148.(Distinct,Where) Article Views I/

---
+++ Miscellaneous/1148.(Distinct,Where) Article Views I/query.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import duckdb
+
+views_data = [
+    [1, 3, 5, "2019-08-01"],
+    [1, 3, 6, "2019-08-02"],
+    [2, 7, 7, "2019-08-01"],
+    [2, 7, 6, "2019-08-02"],
+    [4, 7, 1, "2019-07-22"],
+    [3, 4, 4, "2019-07-21"],
+    [3, 4, 4, "2019-07-21"],
+]
+
+views = pd.DataFrame(
+    views_data,
+    columns=[
+        "article_id",
+        "author_id",
+        "viewer_id",
+        "view_date"
+    ]
+).astype({
+    "article_id": "int64",
+    "author_id": "int64",
+    "viewer_id": "int64",
+    "view_date": "datetime64[ns]"
+})
+
+print(duckdb.query("""
+select distinct viewer_id as Id from views where author_id=viewer_id order by viewer_id asc
+""").to_df())
